56596017b018c6c14d31852d063d28d35b474775
Gustavo <gar.santeliz@gmail.com>
2022-01-29 21:51:27 -0500

    structuring array

---
+++ javascript-quiz-oop/src/component/Answers.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AnswersOptions = [
+    ['A person', 'A plant', 'A reptil', 'A aricraft'],
+    ['Venezuela', 'Brasil', 'USA', 'Germany'],
+    [15, 20, 35, 85]
+]
