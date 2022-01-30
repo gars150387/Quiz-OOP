ef52a2861f81cbdf3c50f574ea6408148545dfa6
Gustavo <gar.santeliz@gmail.com>
2022-01-30 15:47:55 -0500

    adding functionality to buttons

--- javascript-quiz-oop/src/component/Card.js
+++ javascript-quiz-oop/src/component/Card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button, ListGroup } from 'react-bootstrap'
 import { Options } from './Options';
@@ -7,19 +7,31 @@ import { FooterCounter } from './Footer';
 
 
 export const CardTemplate = () => {
+
+    const [plus, setPlus] = useState(0)
+
+    let index = 0;
+
     return (
         <>
-            <Card id="card" className="text-center">
+            <Card id="card" className="text-center" style={{ width: '35rem' }}>
                 <Card.Header style={{color: 'black'}} >Quiz</Card.Header>
                 <Card.Body>
                     <Card.Title style={{color: 'black'}}><QuestionsRender/></Card.Title>
-                    <Card style={{ width: '18rem' }}>
+                    <Card >
                         <ListGroup variant="flush">
                         <Options />
                         </ListGroup>
                     </Card>
-                    <Button variant="primary">Reset</Button>
-                    <Button variant="primary">Skip</Button>
+                    <Button onClick={ element => setPlus(
+                        function(index){
+                            index = 0;
+                            console.log('index',index)
+                        }
+                    )} variant="primary">Reset</Button>
+                    <Button onClick={ element => setPlus(
+                        console.log(index++)
+                    )} variant="primary">Skip</Button>
                 </Card.Body>
                 <FooterCounter />
             </Card>
