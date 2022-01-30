86cefa1367c6a8fe7c576aacae06f7cdf93ebffd
Gustavo <gar.santeliz@gmail.com>
2022-01-29 21:47:23 -0500

    structuring card adding components

--- javascript-quiz-oop/src/component/Card.js
+++ javascript-quiz-oop/src/component/Card.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button, ListGroup } from 'react-bootstrap'
+import { Options } from './Options';
+import { QuestionsRender } from './Questions';
+import { FooterCounter } from './Footer';
 
 
 export const CardTemplate = () => {
@@ -9,19 +12,16 @@ export const CardTemplate = () => {
             <Card id="card" className="text-center">
                 <Card.Header style={{color: 'black'}} >Quiz</Card.Header>
                 <Card.Body>
-                    <Card.Title style={{color: 'black'}}>Questions</Card.Title>
+                    <Card.Title style={{color: 'black'}}><QuestionsRender/></Card.Title>
                     <Card style={{ width: '18rem' }}>
                         <ListGroup variant="flush">
-                            <ListGroup.Item>option 1</ListGroup.Item>
-                            <ListGroup.Item>option 2</ListGroup.Item>
-                            <ListGroup.Item>option 3</ListGroup.Item>
-                            <ListGroup.Item>option 4</ListGroup.Item>
+                        <Options />
                         </ListGroup>
                     </Card>
                     <Button variant="primary">Reset</Button>
                     <Button variant="primary">Skip</Button>
                 </Card.Body>
-                <Card.Footer className="text-muted">Question x of y</Card.Footer>
+                <FooterCounter />
             </Card>
 
         </>
