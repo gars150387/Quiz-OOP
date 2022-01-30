c26d5b2b74351aab3a78de8e3b7d627b8613eaaa
Gustavo <gar.santeliz@gmail.com>
2022-01-29 21:48:10 -0500

    structuring dynamic component

---
+++ javascript-quiz-oop/src/component/Footer.js
@@ -0,0 +1,17 @@
+import { QuestionsOptions } from "./Data"
+import { Card } from "react-bootstrap"
+
+
+export const FooterCounter = () =>{
+    let index = 0
+
+    return (
+        <>
+        {QuestionsOptions && QuestionsOptions.map(question =>{
+            return(
+                <Card.Footer className="text-muted">{`Question ${index} of ${QuestionsOptions.length}`}</Card.Footer>
+            )
+        })}
+        </>
+    )
+}
