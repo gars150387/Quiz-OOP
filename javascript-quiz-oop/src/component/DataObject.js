860e21e7c40f4b2c5f3192315f613b79876c4014
Gustavo <gar.santeliz@gmail.com>
2022-01-30 15:39:58 -0500

    structuring data into an object

---
+++ javascript-quiz-oop/src/component/DataObject.js
@@ -0,0 +1,15 @@
+export const QuestionsAndAnswer = [{
+    question: 'Who is Gustavo',
+    choices: ['a person', 'a plant', 'a object'],
+    answer: 'a person'
+},
+{
+    question: 'Where is he coming from',
+    choices: ['venezuela', 'argentina', 'brasil'],
+    answer: 'venezuela'
+},
+{
+    question: 'How old is he',
+    choices: ['16', '20', '85'],
+    answer: '85'
+}]
