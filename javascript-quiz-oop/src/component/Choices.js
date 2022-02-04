a0b10bc58b1080b3b701c83d7379f667b8c28c92
Gustavo <gar.santeliz@gmail.com>
2022-02-03 19:13:42 -0500

    component name changed

---
+++ javascript-quiz-oop/src/component/Choices.js
@@ -0,0 +1,6 @@
+export const AnswersOptions = [
+    ['A person', 'A plant', 'A reptil', 'A aricraft'],
+    ['Venezuela', 'Brasil', 'USA', 'Germany'],
+    [15, 20, 35, 85]
+]
+console.log(AnswersOptions)
