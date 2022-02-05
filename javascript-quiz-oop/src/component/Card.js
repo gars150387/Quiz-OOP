ec3166af97a2f0d2640257d02619cdb0fdd4a79c
Gustavo <gar.santeliz@gmail.com>
2022-02-04 19:08:04 -0500

    eliminate components like questions buttons and footer

--- javascript-quiz-oop/src/component/Card.js
+++ javascript-quiz-oop/src/component/Card.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button, ListGroup } from 'react-bootstrap'
-import { Options } from './Options';
-import { QuestionsRender } from './Questions';
-import { FooterCounter } from './Footer';
+import { Card } from 'react-bootstrap'
+import {Options} from './ChoicesRender'
 
 
 export const CardTemplate = () => {
@@ -14,26 +12,8 @@ export const CardTemplate = () => {
 
     return (
         <>
-            <Card id="card" className="text-center" style={{ width: '35rem' }}>
-                <Card.Header style={{color: 'black'}} >Quiz</Card.Header>
-                <Card.Body>
-                    <Card.Title style={{color: 'black'}}><QuestionsRender/></Card.Title>
-                    <Card >
-                        <ListGroup variant="flush">
-                        <Options />
-                        </ListGroup>
-                    </Card>
-                    <Button onClick={ element => setPlus(
-                        function(index){
-                            index = 0;
-                            console.log('index',index)
-                        }
-                    )} variant="primary">Reset</Button>
-                    <Button onClick={ element => setPlus(
-                        console.log(index++)
-                    )} variant="primary">Skip</Button>
-                </Card.Body>
-                <FooterCounter />
+            <Card id="card" className="text-center" style={{ width: '85rem' }}>
+                <Options />
             </Card>
 
         </>
