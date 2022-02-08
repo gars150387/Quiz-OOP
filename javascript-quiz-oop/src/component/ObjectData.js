c9e81af09d6d1cf5c717e78f05173f24dff065e7
Gustavo <gar.santeliz@gmail.com>
2022-02-07 19:35:48 -0500

    data base structure

---
+++ javascript-quiz-oop/src/component/ObjectData.js
@@ -0,0 +1,13 @@
+export const ObjectData = [{
+    question: "who is Gustavo",
+    choice: ["a person", "a aircraft", "a bird", "a plant"],
+    answer: "a person"
+},{
+    question: "Where is he coming from",
+    choice: ["venezuela", "a aircraft", "a bird", "a plant"],
+    answer: "venezuela"
+},{
+    question: "how old is he",
+    choice: ["34", "a aircraft", "a bird", "a plant"],
+    answer: "34"
+}]
