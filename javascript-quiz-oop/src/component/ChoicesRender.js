2aaf53804fcaa647def54c85ad752b23bb8c427f
Gustavo <gar.santeliz@gmail.com>
2022-02-09 18:38:19 -0500

    structuring component with other components

--- javascript-quiz-oop/src/component/ChoicesRender.js
+++ javascript-quiz-oop/src/component/ChoicesRender.js
@@ -2,38 +2,43 @@ import React, { useState } from "react";
 import { ListGroup } from "react-bootstrap";
 import { AnswersOptions } from "./Choices";
 import { FooterCounter } from "./Footer";
-import { QuestionsRender } from './Questions'
-import { Button } from "react-bootstrap";
+// import { QuestionsRender } from './Questions'
+import { Button, Card } from "react-bootstrap";
+import { state } from './Actions'
+import { QuestionsOptions } from "./QuestionsData";
 
 export const Options = () => {
-    const [clicked, setClicked] = useState(false)
-    const [plus, setPlus] = useState(0)
 
-    let index = 0
+    const [setClicked] = useState('')
+    const [increase, setIncrease] = useState(0)
+    const [reset, setReset] = useState(false)
+
+    let index = increase
+    console.log('index', increase)
 
     return (
         <>
-            <QuestionsRender index={index} />
+
+            <Card.Footer key={QuestionsOptions[index][index]} style={{ color: 'black' }}>{QuestionsOptions[index]}</Card.Footer>
+            {/* comment component  */}
+            {/* <QuestionsRender index={index} /> */}
             {AnswersOptions[index] && AnswersOptions[index].map((answer) => {
                 return (
                     <ListGroup.Item onClick={e => setClicked(
                         console.log("cliked"), []
-                    )} key={answer[index]} >{answer}</ListGroup.Item>
+                    )}key={answer[index][index]} >{answer}</ListGroup.Item>
                 )
             })
             }
-            <Button onClick={element => setPlus(
-                function (index) {
-                    index = 0;
-                    console.log('index', index)
-                }
-            )} variant="primary" className="mr-3">Reset</Button>
-            <Button onClick={element => setPlus(
+            <Button className="bg-danger m-auto" style={{height: '50px', color: 'white', width: '75%'}} onClick={()=> setIncrease(
+                index++,
+                console.log('increase', index)
+            )}>SKIP</Button>
+            <Button className="bg-success m-auto" style={{height: '50px', color: 'black', width: '75%'}} >RESET</Button>
 
-                console.log(index++)
-            )} variant="primary">Skip</Button>
-
-            <FooterCounter />
+            <Card.Footer key={QuestionsOptions[index][index]} className="text-muted">Question{` ${index + 1} of ${QuestionsOptions.length}`}</Card.Footer>
+          
+            {/* <FooterCounter /> */}
         </>
     )
 }
