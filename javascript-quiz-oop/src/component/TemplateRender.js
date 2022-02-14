ed3ed73be4ebf86e4991f5400d5e2cbbdaea96f0
Gustavo <gar.santeliz@gmail.com>
2022-02-13 21:02:11 -0500

    render elements in object

--- javascript-quiz-oop/src/component/TemplateRender.js
+++ javascript-quiz-oop/src/component/TemplateRender.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { ObjectData } from './ObjectData'
+import { ListGroup } from 'react-bootstrap'
 
-export const TemplateToRenderData = ()=>{
-return (
-    <>
-    {ObjectData.question}
-    {ObjectData.map((item)=>{
-        return <li>{item.choice}</li>
-    })}
-    </>
-)
+export const TemplateToRenderData = () => {
+
+    return (
+        <>
+            {ObjectData.map((choice, index) => {
+                return (
+                    
+                    <ListGroup.Item>
+                        <h2 style={{ color: 'black' }} index={index} key={choice}>
+                            {choice.choice}
+                        </h2>
+                    </ListGroup.Item>
+                )
+
+            })}
+        </>
+    )
 }
