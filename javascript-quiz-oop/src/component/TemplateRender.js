eee6f6ad0780b9152d3b4183e0917a415779375f
Gustavo <gar.santeliz@gmail.com>
2022-02-15 18:56:48 -0500

    adding buttons and functionality

--- javascript-quiz-oop/src/component/TemplateRender.js
+++ javascript-quiz-oop/src/component/TemplateRender.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { ObjectData } from './ObjectData'
-import { ListGroup } from 'react-bootstrap'
+import { Button, ListGroup } from 'react-bootstrap'
 import { TemplateToRenderQuestion } from './TemplateRenderQuestion'
 import { TemplateToRenderAnswer } from './TemplateToRenderAnswer'
 
 export const TemplateToRenderData = () => {
-    let index = 0
-    console.log('index', index)
+    let index = null
+
+    function Clicked(){
+        let index = index + 1;
+        console.log('clicked', index)
+    }
+
 
     return (
         <>
         <TemplateToRenderQuestion />
         {console.log(TemplateToRenderQuestion)}
-            {ObjectData.map((choice) => {
+            {ObjectData.map((choice,value) => {
                 return (
                     
                     <ListGroup.Item>
-                        <h2 style={{ color: 'black' }} index={index} key={choice}>
+                        <h2 style={{ color: 'black' }} index={value} key={choice}>
                             {choice.choice}
                         </h2>
                     </ListGroup.Item>
@@ -24,6 +29,9 @@ export const TemplateToRenderData = () => {
 
             })}
             <TemplateToRenderAnswer/>
+            <div className='row'>
+            <Button className='m-auto bg-success col-4' style={{height: '35px', display: '-ms-flexbox'}} onClick={Clicked} >SKIP</Button><Button className='m-auto bg-success col-4' style={{height: '35px'}}></Button>
+            </div> 
         </>
     )
 }
