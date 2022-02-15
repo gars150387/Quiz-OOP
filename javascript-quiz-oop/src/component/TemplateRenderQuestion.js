f4a34a8068e6c3eeeade87615e9a4f0e8121df37
Gustavo <gar.santeliz@gmail.com>
2022-02-15 18:57:45 -0500

    re writing component to pass value

--- javascript-quiz-oop/src/component/TemplateRenderQuestion.js
+++ javascript-quiz-oop/src/component/TemplateRenderQuestion.js
@@ -6,12 +6,12 @@ export const TemplateToRenderQuestion = () => {
 
     return (
         <>
-           {ObjectData.map((question, index) => {
+           {ObjectData.map((question, index, value) => {
                index= 0
                 return (
                     
                     <ListGroup.Item>
-                        <h2 style={{ color: 'black' }} index={index} key={question}>
+                        <h2 style={{ color: 'black' }} index={value} key={question}>
                             {question.question}
                         </h2>
                     </ListGroup.Item>
