1333f5f35ef6876ee099e643d2f8ccea1f86364e
Gustavo <gar.santeliz@gmail.com>
2022-02-15 19:03:43 -0500

    structuring styling

--- javascript-quiz-oop/src/component/TemplateRender.js
+++ javascript-quiz-oop/src/component/TemplateRender.js
@@ -7,31 +7,38 @@ import { TemplateToRenderAnswer } from './TemplateToRenderAnswer'
 export const TemplateToRenderData = () => {
     let index = null
 
-    function Clicked(){
-        let index = index + 1;
+    function Clicked() {
+        index++
         console.log('clicked', index)
     }
 
 
     return (
         <>
-        <TemplateToRenderQuestion />
-        {console.log(TemplateToRenderQuestion)}
-            {ObjectData.map((choice,value) => {
-                return (
-                    
-                    <ListGroup.Item>
-                        <h2 style={{ color: 'black' }} index={value} key={choice}>
-                            {choice.choice}
-                        </h2>
-                    </ListGroup.Item>
-                )
-
-            })}
-            <TemplateToRenderAnswer/>
             <div className='row'>
-            <Button className='m-auto bg-success col-4' style={{height: '35px', display: '-ms-flexbox'}} onClick={Clicked} >SKIP</Button><Button className='m-auto bg-success col-4' style={{height: '35px'}}></Button>
-            </div> 
+                <TemplateToRenderQuestion />
+            </div>
+            <div className='row'>
+                {console.log(TemplateToRenderQuestion)}
+                {ObjectData.map((choice, value) => {
+                    return (
+
+                        <ListGroup.Item>
+                            <h2 style={{ color: 'black' }} index={value} key={choice}>
+                                {choice.choice}
+                            </h2>
+                        </ListGroup.Item>
+                    )
+
+                })}
+            </div>
+            <div className='row'>
+                <TemplateToRenderAnswer />
+            </div>
+
+            <div className='row'>
+                <Button className='m-auto bg-success col-4' style={{ height: '35px', display: '-ms-flexbox' }} onClick={Clicked} >SKIP</Button><Button className='m-auto bg-success col-4' style={{ height: '35px' }}></Button>
+            </div>
         </>
     )
 }
