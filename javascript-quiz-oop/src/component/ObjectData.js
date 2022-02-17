4a20a748aa00a9ab2bbc4d4a9b2da27438773902
Gustavo <gar.santeliz@gmail.com>
2022-02-16 20:05:54 -0500

    fixing structure of the objet

--- javascript-quiz-oop/src/component/ObjectData.js
+++ javascript-quiz-oop/src/component/ObjectData.js
@@ -1,13 +1,20 @@
 export const ObjectData = [{
-    question: "who is Gustavo",
-    choice: [" person ", " aircraft ", " bird ", " plant "],
-    answer: "a person"
-},{
-    question: "Where is he coming from",
-    choice: ["venezuela ",'uruguay ', 'brasil ', 'argentina '],
-    answer: "venezuela"
-},{
-    question: "how old is he",
-    choice: ["34 ", '55 ', '85 ', '20'],
-    answer: "34"
+    option1: {
+        question: "who is Gustavo",
+        choice: [" person ", " aircraft ", " bird ", " plant "],
+        answer: "a person"
+    }
+}, {
+    option2: {
+        question: "Where is he coming from",
+        choice: ["venezuela ", 'uruguay ', 'brasil ', 'argentina '],
+        answer: "venezuela"
+    }
+}, {
+    option3: {
+        question: "how old is he",
+        choice: ["34 ", '55 ', '85 ', '20'],
+        answer: "34"
+    }
 }]
+
