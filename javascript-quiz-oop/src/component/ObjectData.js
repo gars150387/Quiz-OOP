5cbbf2910fca4798b8941b18d08fc32ad08b2ebd
Gustavo <gar.santeliz@gmail.com>
2022-03-11 19:20:56 -0500

    including questions

--- javascript-quiz-oop/src/component/ObjectData.js
+++ javascript-quiz-oop/src/component/ObjectData.js
@@ -1,5 +1,5 @@
 export const objectData = [{
-    question: "who is Gustavo",
+    question: "who is Alfredo",
     choice: [" person ", " aircraft ", " bird ", " plant "],
     answer: "a person"
 }, {
@@ -9,6 +9,50 @@ export const objectData = [{
 }, {
     question: "how old is he",
     choice: ["34 ", '55 ', '85 ', '20'],
-    answer: "34"
+    answer: "35"
+}, {
+    question: "what is his favorite number",
+    choice: ["7 ", '55 ', '85 ', '20'],
+    answer: "7"
+}, {
+    question: "what is his favorite food",
+    choice: ["pizza ", '55 ', '85 ', '20'],
+    answer: "pizza"
+}, {
+    question: "how many sibbling does he have",
+    choice: ["3 ", '55 ', '85 ', '20'],
+    answer: "3"
+}, {
+    question: "what cellphone does he have",
+    choice: ["iphone ", '55 ', '85 ', '20'],
+    answer: "iphone"
+}, {
+    question: "what is his biggest degree achieved",
+    choice: ["bachelor ", '55 ', '85 ', '20'],
+    answer: "bachelor"
+}, {
+    question: "what is the name is his middle school",
+    choice: ["carlota ", '55 ', '85 ', '20'],
+    answer: "carlota"
+}, {
+    question: "where did he study",
+    choice: ["san cristobal ", '55 ', '85 ', '20'],
+    answer: "san cristobal"
+}, {
+    question: "where had not he worked",
+    choice: ["saturn ", '55 ', '85 ', '20'],
+    answer: "saturn"
+}, {
+    question: "how tall is he",
+    choice: ["8 inches ", '55 ', '85 ', '20'],
+    answer: "8 inches"
+}, {
+    question: "what are his hobbies",
+    choice: ["domino ", '55 ', '85 ', '20'],
+    answer: "domino"
+}, {
+    question: "what is his civil state",
+    choice: ["married ", '55 ', '85 ', '20'],
+    answer: "married"
 }]
 
